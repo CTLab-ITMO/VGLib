71d14a83d8beed58c14c039633fd13272d2afb9f
Artem <334947@niuitmo.ru>
2024-05-31 19:04:13 +0300

    Added Vector-NST

---
+++ Vector-neuro-style-transfer/README.md
@@ -0,0 +1,35 @@
+# VectorNST
+
+<div align="center">
+  <img src="images/owl_stylized_owl1.jpg" alt="img1" width="512" height="160"/>
+  <img src="images/scene6_stylized_scene8.jpg" alt="img2" width="512" height="130"/>
+  <img src="images/flower.jpg" alt="img3" width="512" height="160"/>
+</div>
+
+The official implementation of VectorNST (vector neural style transfer) model in PyTorch.
+
+[Demo service](http://81.3.154.178:5001/vector_style_transfer).
+
+## Usage
+
+### Requirements
+
+Install these requirements:
+
+* [PyTorch](https://pytorch.org) and Torchvision
+* Pillow
+* NumPy
+* [DiffVG](https://github.com/IzhanVarsky/diffvg) and its requirements
+
+Example steps of installing:
+
+* `pip install torch torchvision pillow numpy`
+* `pip install cssutils scikit-learn scikit-image svgwrite svgpathtools matplotlib`
+* `git clone --recursive https://github.com/IzhanVarsky/diffvg`
+* `cd diffvg && python ./setup.py install && cd ..`
+
+### Testing
+
+Run our example `python test_vector_nst.py --content_img ./images/owl.svg --style_img ./images/owl1.jpg`.
+
+You can also specify some other parameters: run `python test_vector_nst.py --help` to see more info.
---
+++ Vector-neuro-style-transfer/joint_loss.py
@@ -0,0 +1,117 @@
+from math import sqrt
+
+import bezier
+import numpy as np
+import torch
+
+class JointLoss(torch.nn.Module):
+
+    def get_coordinates(self, t, points):
+        curve = bezier.Curve(points, degree=3)
+        return curve.evaluate(t)
+
+
+    def is_local_max(self, t, curve):
+        eps = 0.1
+        return curve.evaluate(t)[1][0] > curve.evaluate(t + eps)[1][0] and curve.evaluate(t)[1][0] > \
+            curve.evaluate(t-eps)[1][0]
+
+
+    def is_local_min(self, t, curve):
+        eps = 0.1
+        return curve.evaluate(t)[1][0] < curve.evaluate(t + eps)[1][0] and curve.evaluate(t)[1][0] < \
+            curve.evaluate(t - eps)[1][0]
+
+
+    class AnchorPoints:
+        a0, b0 = 0, 0
+        a1, b1 = 0, 0
+        a2, b2 = 0, 0
+        a3, b3 = 0, 0
+
+        def __init__(self, points: np.ndarray):
+            self.a0, self.b0 = points[0][0], points[0][1]
+            self.a1, self.b1 = points[1][0], points[1][1]
+            self.a2, self.b2 = points[2][0], points[2][1]
+            self.a3, self.b3 = points[3][0], points[3][1]
+
+        def swap_points(self):
+            ap = self
+            ap.a0, ap.b0 = self.b0, self.a0
+            ap.a1, ap.b1 = self.b1, self.a1
+            ap.a2, ap.b2 = self.b2, self.a2
+            ap.a3, ap.b3 = self.b3, self.a3
+            return ap
+
+        def generate_curve(self):
+            return bezier.Curve(np.asfortranarray([
+                [self.a0, self.a1, self.a2, self.a3],
+                [self.b0, self.b1, self.b2, self.b3]
+            ]), degree=3)
+
+
+    def find_joint(self, points, anchor_points):
+        curve = anchor_points.generate_curve()
+        a0 = anchor_points.a0
+        a1 = anchor_points.a1
+        a2 = anchor_points.a2
+        a3 = anchor_points.a3
+        k = a2 - 2 * a1 + a0
+        a = a3 - 3 * a2 + 3 * a1 - a0
+        c = a1 - a0
+        if a == 0:
+            swapped_points = np.swapaxes(points, 0, 1)
+            swapped_ap = anchor_points.swap_points()
+            swapped_point, t_res = self.find_joint(points=swapped_points, anchor_points=swapped_ap)
+            if t_res == -1:
+              return np.ndarray(shape=(0, 0)), -1
+            return np.array([swapped_point[1], swapped_point[0]]), t_res
+        d1 = k * k - a * c
+        if d1 < 0:
+            return np.ndarray(shape=(0, 0)), -1
+        t1 = (-1 * k + sqrt(d1)) / a
+        t2 = (-1 * k - sqrt(d1)) / a
+        if 1 >= t1 >= 0:
+            if self.is_local_max(t1, curve) or self.is_local_min(t1, curve):
+                return curve.evaluate(t1), t1
+        if 1 >= t2 >= 0:
+            if self.is_local_max(t2, curve) or self.is_local_min(t2, curve):
+                return curve.evaluate(t2), t2
+        return np.ndarray(shape=(0, 0)), -1
+
+
+    def joint_loss(self, points: torch.Tensor, eps=0.1):
+      # print(points)
+      np_points = points.detach().numpy()
+      # print(np_points)
+      ap = self.AnchorPoints(np_points)
+      joint, t_joint = self.find_joint(np_points, ap)
+      if t_joint == -1:
+          return 0
+
+      def der_x(t): return 3*ap.a3*t*t - 3*ap.a2*(3*t*t - 2*t) + 3*ap.a1*(3*t*t - 4*t + 1) - 3*ap.a0*(t*t - 2*t + 1)
+      def der_y(t): return 3*ap.b3*t*t - 3*ap.b2*(3*t*t - 2*t) + 3*ap.b1*(3*t*t - 4*t + 1) - 3*ap.b0*(t*t - 2*t + 1)
+      def double_der_x(t): return 6*t*(ap.a3 - 3*ap.a2 + 3*ap.a1 - ap.a0) + 6*(ap.a2 - 2*ap.a1 + ap.a0)
+      def double_der_y(t): return 6*t*(ap.b3 - 3*ap.b2 + 3*ap.b1 - ap.b0) + 6*(ap.b2 - 2*ap.b1 + ap.b0)
+
+      def k(t):
+          x_ap = der_x(t)
+          y_ap = der_y(t)
+          x_ap_ap = double_der_x(t)
+          y_ap_ap = double_der_y(t)
+          norm = sqrt(x_ap * x_ap + y_ap * y_ap)
+          if norm == 0: return 0.
+          return (x_ap * y_ap_ap - x_ap_ap * y_ap) / (norm * norm * norm)
+
+      k_left = k(max(t_joint - eps, 0))
+      k_right = k(min(t_joint + eps, 1))
+      return abs(k_left) + abs(k_right)
+
+    def forward(self, x_list, eps=0.1):
+      loss = 0.
+      # print(len(x_list))
+      for x in x_list:
+        # print(x)
+        # area_loss * scale
+        loss += self.joint_loss(x)
+      return torch.tensor(loss)
